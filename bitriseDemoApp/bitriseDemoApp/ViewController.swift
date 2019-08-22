d14c33f0570a12e64016ca83e6c92ab1d9c2a5a2
Javier Calatrava <javiercalatrava@capitole-consulting.com>
2019-08-22 20:38:13 +0200

    Blank single view project

---
+++ bitriseDemoApp/bitriseDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bitriseDemoApp
+//
+//  Created by Javier Calatrava on 22/08/2019.
+//  Copyright © 2019 Javier Calatrava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bitriseDemoApp/bitriseDemoAppTests/bitriseDemoAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  bitriseDemoAppTests.swift
+//  bitriseDemoAppTests
+//
+//  Created by Javier Calatrava on 22/08/2019.
+//  Copyright © 2019 Javier Calatrava. All rights reserved.
+//
+
+import XCTest
+@testable import bitriseDemoApp
+
+class bitriseDemoAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
